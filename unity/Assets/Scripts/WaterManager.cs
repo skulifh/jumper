b2ddbc645316e31dd92a760e1d0629d325945a6d
Skúli Freyr Hinriksson <skulifh@bitbucket.org>
2014-12-11 19:46:07 +0100

    changes

--- unity/Assets/Scripts/WaterManager.cs
+++ unity/Assets/Scripts/WaterManager.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 
 public class WaterManager : MonoBehaviour {
 	public float waterSurface, leftWall, rightWall;
+	public int minLength, maxLength;
 	// Use this for initialization
 	void Start () {
 		//this.transform.position = new Vector3(5, -6, 5);
+		int length = Random.Range(minLength, maxLength);
+		this.transform.Find("Bottom").transform.localScale = new Vector3(length, 1, 1);
+		this.transform.Find("Bottom").transform.localPosition = new Vector3(((this.transform.Find("Bottom").transform.localScale.x)/2)-4, -12, 0);
 		waterSurface = this.transform.Find("Water").transform.localPosition.y + (this.transform.Find("Water").transform.localScale.y)/2;
 		leftWall = this.transform.Find("Left wall").transform.position.x;
 		rightWall = this.transform.Find("Right wall").transform.position.x;
 		Debug.Log(leftWall);
+		Debug.Log(transform.position);
 		//Debug.Log(Player.currentPosition.x);
 		//Debug.Log(this.transform.Find("Water").transform.localScale);
 		//Debug.Log(Player.currentPosition);
