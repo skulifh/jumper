b145f9342def1c9c32ff61293c21d590a1177e85
freko <freko247@gmail.com>
2014-12-13 09:37:17 +0100

    Added flying enemy.

---
+++ unity/Assets/Scripts/FlyerStrategy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyerStrategy : MonoBehaviour {
+	private static Vector3 startPosition;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > startPosition.x + 5){
+			speed = -speed;
+		}
+		else if(transform.position.x < startPosition.x - 5){
+			speed = -speed;
+		}
+		transform.Translate (speed, 0, 0);
+	}
+}
