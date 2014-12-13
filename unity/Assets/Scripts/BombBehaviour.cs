d33ddded7068145e3a7bb32a037cb0fc0bc70bf7
freko <freko247@gmail.com>
2014-12-13 20:20:13 +0100

    Flyers can now release bombs.

---
+++ unity/Assets/Scripts/BombBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehaviour : MonoBehaviour {
+
+	public int destroyDepth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.localPosition.y < -10){
+			Destroy(gameObject);
+			}
+	}
+
+	void OnTriggerEnter () {
+		Destroy(gameObject);
+	}
+}
